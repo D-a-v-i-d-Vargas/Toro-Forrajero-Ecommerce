1947cfa1c8115760f54d1813a62c7643edd861b0
Daniela Tobón <daniela.tobon@anahuac.mx>
2026-08-17 17:35:35 -0600

    creación de detalles pedidos

---
+++ database/DML_detallePedido.sql
@@ -0,0 +1,44 @@
+INSERT INTO detalle_pedido (id_pedido, id_Producto, cantidad, precio_unitario, subtotal) VALUES
+-- Pedido Bovinos 
+(1, 1, 2, 300.00, 600.00),   -- 2x Mezcla Nutridor 25kg
+(1, 2, 1, 279.00, 279.00),   -- 1x Mezcla Ganadera 25kg
+(1, 9, 1, 410.00, 410.00),   -- 1x Engorda Ganado 25kg
+
+-- Pedido Porcinos
+(2, 5, 3, 286.00, 858.00),   -- 3x Finalizador® Engorda Cerdos H.L. 25kg
+(2, 6, 2, 286.00, 572.00),   -- 2x Growpig!® Desarrollo 25kg
+
+-- Pedido Aves
+(3, 3, 5, 245.00, 1225.00),  -- 5x Pollo Especial 25kg
+(3, 4, 2, 245.00, 490.00),   -- 2x Nutridor Pollos 25kg
+(3, 11, 1, 438.00, 438.00),  -- 1x Fortipollo (iniciador) 25kg
+
+-- Pedido Ovinos
+(4, 7, 2, 269.00, 538.00),   -- 2x Borregos Forte 25kg
+(4, 8, 3, 269.00, 807.00),   -- 3x Borrego Ganador 25kg
+(4, 15, 1, 525.00, 525.00),  -- 1x Preiniciador Borrego 25kg
+
+-- Pedido Bovinos Arandas
+(5, 17, 2, 750.00, 1500.00), -- 2x Ara H Engorda Ganado 12% Rol 25kg
+(5, 18, 1, 750.00, 750.00),  -- 1x Ara P Crecimiento Becerra 25kg
+
+-- Pedido Porcinos mixto
+(6, 13, 4, 369.00, 1476.00), -- 4x Engorda 25kg
+(6, 21, 2, 750.00, 1500.00), -- 2x Ara H Crecicerdos 25kg
+
+-- Pedido Aves mixto
+(7, 12, 3, 245.00, 735.00),  -- 3x Maxipollo (engorda) 25kg
+(7, 19, 2, 770.00, 1540.00), -- 2x Ara M Inipollo 25kg
+
+-- Pedido Ovinos Arandas
+(8, 23, 2, 750.00, 1500.00), -- 2x Ara P Borrego Engorda 25 KG
+(8, 24, 1, 750.00, 750.00),  -- 1x Ara H Borrego Engorda Rol Plus 25kg
+
+-- Pedido Compra Mixta
+(9, 1, 3, 300.00, 900.00),   -- 3x Mezcla Nutridor 25kg
+(9, 10, 2, 410.00, 820.00),  -- 2x Concentrado para Engorda Ganado 25kg
+
+-- Pedido Compra mixta
+(10, 2, 2, 279.00, 558.00),  -- 2x Mezcla Ganadera 25kg
+(10, 14, 1, 644.00, 644.00), -- 1x Crecimiento 25kg
+(10, 20, 1, 770.00, 770.00); -- 1x Ara Sostenedor 25kg
