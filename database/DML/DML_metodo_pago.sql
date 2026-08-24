6f51f512e0c43028024d203d08f8b619f8c63372
Daniela Tobón <daniela.tobon@anahuac.mx>
2026-08-24 12:25:10 -0600

    Fix: Se corrigió la fecha en método de pago

--- database/DML/DML_metodo_pago.sql
+++ database/DML/DML_metodo_pago.sql
@@ -5,17 +5,16 @@
 -- =================================================================================================
 
 INSERT INTO metodo_pago (num_tarjeta, fecha_expiracion, id_usuario) VALUES 
-	(HEX(AES_ENCRYPT('8364506782364758', 'mi_clave_secreta')), '2028-06', 1),
-	(HEX(AES_ENCRYPT('4152313485960123', 'mi_clave_secreta')), '2027-11', 2),
-	(HEX(AES_ENCRYPT('5284910238475612', 'mi_clave_secreta')), '2029-08', 3),
-	(HEX(AES_ENCRYPT('4916238471029384', 'mi_clave_secreta')), '2026-03', 4),
-	(HEX(AES_ENCRYPT('5412983746501928', 'mi_clave_secreta')), '2028-12', 5),
-	(HEX(AES_ENCRYPT('4026183940512837', 'mi_clave_secreta')), '2030-05', 6),
-	(HEX(AES_ENCRYPT('5105293847162039', 'mi_clave_secreta')), '2027-09', 7),
-	(HEX(AES_ENCRYPT('4532819023847162', 'mi_clave_secreta')), '2029-01', 8),
-	(HEX(AES_ENCRYPT('5321894726103847', 'mi_clave_secreta')), '2028-07', 9),
-	(HEX(AES_ENCRYPT('4820193847562910', 'mi_clave_secreta')), '2026-10', 10);
-
+	(HEX(AES_ENCRYPT('8364506782364758', 'mi_clave_secreta')), '06/2028', 1),
+	(HEX(AES_ENCRYPT('4152313485960123', 'mi_clave_secreta')), '11/2027', 2),
+	(HEX(AES_ENCRYPT('5284910238475612', 'mi_clave_secreta')), '08/2029', 3),
+	(HEX(AES_ENCRYPT('4916238471029384', 'mi_clave_secreta')), '03/2026', 4),
+	(HEX(AES_ENCRYPT('5412983746501928', 'mi_clave_secreta')), '12/2028', 5),
+	(HEX(AES_ENCRYPT('4026183940512837', 'mi_clave_secreta')), '05/2030', 6),
+	(HEX(AES_ENCRYPT('5105293847162039', 'mi_clave_secreta')), '09/2027', 7),
+	(HEX(AES_ENCRYPT('4532819023847162', 'mi_clave_secreta')), '01/2029', 8),
+	(HEX(AES_ENCRYPT('5321894726103847', 'mi_clave_secreta')), '07/2028', 9),
+	(HEX(AES_ENCRYPT('4820193847562910', 'mi_clave_secreta')), '10/2026', 10);
 
 
 -- =================================================================================================
